8109f248fe644600070a6208b5bbda6174f0cdb4
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-06 16:28:47 -0800

    resume data showing in console successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import {resumeData} from './data/resume.js'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  console.log(resumeData);
 
   return (
     <div className="App">
