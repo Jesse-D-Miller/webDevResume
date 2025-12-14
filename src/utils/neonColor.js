8a38696ac1c99ad7b1944f5914cc753419f0567a
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-14 14:57:32 -0800

    some css and colour selections

--- src/utils/neonColor.js
+++ src/utils/neonColor.js
@@ -5,12 +5,16 @@ export function getRandomNeonColor(element) {
   if (existingColor) return; // Already set for this box
 
   const colors = [
-    { rgb: "255, 0, 0", opacity: "rgba(255, 0, 0, 0.3)" },
-    { rgb: "0, 212, 255", opacity: "rgba(0, 212, 255, 0.3)" },
-    { rgb: "0, 255, 65", opacity: "rgba(0, 255, 65, 0.3)" },
-    { rgb: "255, 255, 0", opacity: "rgba(255, 255, 0, 0.3)" },
-    { rgb: "255, 0, 255", opacity: "rgba(255, 0, 255, 0.3)" },
     { rgb: "0, 255, 255", opacity: "rgba(0, 255, 255, 0.3)" },
+    { rgb: "0, 128, 255", opacity: "rgba(0, 174, 255, 0.3)" },
+    { rgb: "64, 0, 255", opacity: "rgba(255, 0, 251, 0.3)" },
+    { rgb: "186, 0, 255", opacity: "rgba(186, 0, 255, 0.3)" },
+    { rgb: "255, 0, 255", opacity: "rgba(255, 0, 255, 0.3)" },
+    { rgb: "0, 0, 255", opacity: "rgba(99, 99, 255, 0.3)" },
+    { rgb: "0, 255, 200", opacity: "rgba(0, 255, 200, 0.3)" },
+    { rgb: "128, 0, 255", opacity: "rgba(128, 0, 255, 0.3)" },
+    { rgb: "0, 255, 128", opacity: "rgba(0, 255, 128, 0.3)" },
+    { rgb: "171, 116, 255", opacity: "rgba(171, 116, 255, 0.3)" },
   ];
   const color = colors[Math.floor(Math.random() * colors.length)];
   target.style.setProperty("--neon-color", `rgb(${color.rgb})`);
