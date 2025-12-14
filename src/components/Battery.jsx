336e3d3b04be2260fded93e53da3d85ad37348bb
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-14 14:23:28 -0800

    work on implementing battery pixel art for the battery cover

--- src/components/Battery.jsx
+++ src/components/Battery.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
-// import batteryCoverSpriteSheet from "../assets/battery_cover_sprite_sheet.png";
+import batteryCoverSpriteSheet from "../assets/batteryCoverSpriteSheet.png";
 
 function Battery({ charge }) {
   const [coverOpen, setCoverOpen] = useState(false);
+  const [animating, setAnimating] = useState(false);
+
+  const handleClick = () => {
+    setAnimating(true);
+    setCoverOpen((open) => !open);
+  };
+
+  const handleAnimationEnd = () => {
+    setAnimating(false);
+  };
 
   return (
-    <div
-      className="battery"
-      onClick={() => setCoverOpen((coverOpen) => !coverOpen)}
-    >
-      <div className={`battery-cover ${coverOpen ? "cover-open" : ""}`} />
+    <div className="battery" onClick={handleClick}>
+      <div
+        className={`battery-cover ${
+          coverOpen ? "cover-open" : "cover-closed"
+        } ${animating ? "cover-animating" : ""}`}
+        style={{backgroundImage: `url(${batteryCoverSpriteSheet})`}}
+        onAnimationEnd={handleAnimationEnd}
+      />
       {Array.from({ length: 12 }).map((_, index) => (
         <div
           key={index}
