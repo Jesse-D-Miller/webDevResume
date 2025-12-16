dc3fea0c7a6f95ee65e7c5e27eb4d95459f80699
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-16 09:51:11 -0800

    final styling on projects and experience sections is basically done now. organised some of the CSS file to be more readable. added an extra div to projects and experience components so i could make the cards a 2 col grid

--- src/components/resume/ExperienceSection.jsx
+++ src/components/resume/ExperienceSection.jsx
@@ -2,15 +2,17 @@ function ExperienceSection({ job, isFront, showHeader }) {
   return (
     <section className={`experience-section${isFront ? " front" : ""}`}>
       {showHeader && <h3>EXPERIENCE</h3>}
-      <h4>
-        {job.company} - {job.role}
-      </h4>
-      <p className="company-period">{job.period}</p>
-      <ul>
-        {job.bullets.map((bullet, i) => (
-          <li key={i}>{bullet}</li>
-        ))}
-      </ul>
+      <div className="section-content">
+        <h4>
+          {job.company} - {job.role}
+        </h4>
+        <p className="company-period">{job.period}</p>
+        <ul>
+          {job.bullets.map((bullet, i) => (
+            <li key={i}>{bullet}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
--- src/components/resume/ProjectsSection.jsx
+++ src/components/resume/ProjectsSection.jsx
@@ -2,13 +2,15 @@ function ProjectsSection({ project, isFront, showHeader }) {
   return (
     <section className={`projects-section${isFront ? " front" : ""}`}>
       {showHeader && <h3>PROJECTS</h3>}
-      <h4>{project.title}</h4>
-      <p className="tech-stack">{project.stack.join(", ")}</p>
-      <ul>
-        {project.highlights.map((highlight, i) => (
-          <li key={i}>{highlight}</li>
-        ))}
-      </ul>
+      <div className="section-content">
+        <h4>{project.title}</h4>
+        <p className="tech-stack">{project.stack.join(", ")}</p>
+        <ul>
+          {project.highlights.map((highlight, i) => (
+            <li key={i}>{highlight}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
