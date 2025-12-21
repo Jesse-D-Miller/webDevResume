4a6a43148ca0ff4ad7ae745aba0afee7da3fd3b8
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-21 14:57:21 -0800

    trying to make analytics work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { useState } from "react";
 import { XPProvider } from "./context/XPProvider.jsx";
 import { SoftSkillsProvider } from "./context/SoftSkillsProvider.jsx";
