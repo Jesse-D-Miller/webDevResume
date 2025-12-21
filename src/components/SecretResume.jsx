52c31511341a9f54dc642fc444847e9ae351cd76
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-12-21 12:02:31 -0800

    css for secret resume mostly done

--- src/components/SecretResume.jsx
+++ src/components/SecretResume.jsx
@@ -1,54 +1,72 @@
+import finneganImg from "../assets/finneganImg.jpeg";
+
+const dogTreat = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#000000ff"
+  >
+    <path d="M380-80q-59 0-99.5-40.5T240-220q0-9 2.5-14t-.5-8q-3-3-8-.5t-14 2.5q-59 0-99.5-40.5T80-380q0-59 40.5-99.5T220-520q23 0 42 6t36 18l166-166q-12-17-18-36t-6-42q0-59 40.5-99.5T580-880q59 0 99.5 40.5T720-740q0 9-2.5 14t.5 8q3 3 8 .5t14-2.5q59 0 99.5 40.5T880-580q0 59-40.5 99.5T740-440q-23 0-42-6t-36-18L496-298q12 17 18 36t6 42q0 59-40.5 99.5T380-80Zm0-80q26 0 43-17t17-43q0-9-2.5-17.5T430-254q-17-24-14-51.5t24-48.5l166-166q21-21 48.5-24t51.5 14q8 5 16.5 7.5T740-520q26 0 43-17t17-43q0-26-17-43t-43-17q-35 0-49-3.5T662-662q-15-15-18.5-29t-3.5-49q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 11 2 18.5t8 15.5q17 24 14 51.5T520-606L354-440q-21 21-48.5 24T254-430q-8-5-16.5-7.5T220-440q-26 0-43 17t-17 43q0 26 17 43t43 17q35 0 49 3.5t29 18.5q15 15 18.5 29t3.5 49q0 26 17 43t43 17Zm100-320Z" />
+  </svg>
+);
+
 function SecretResume({ secretResumeData }) {
   return (
     <div className="secret-resume">
       <div className="secret-header">
-        <h2>{secretResumeData.meta.name}</h2>
-        <p>{secretResumeData.meta.location}</p>
-        <p>{secretResumeData.meta.links.treat}</p>
+        <h1>{secretResumeData.meta.name}</h1>
+        <p>
+          {secretResumeData.meta.location} | {secretResumeData.meta.links.treat}{" "}
+          | {dogTreat}
+        </p>
       </div>
 
       <div className="secret-summary">
         <h3>Summary</h3>
-        {secretResumeData.summary[0]}
+        <p>{secretResumeData.summary[0]}</p>
       </div>
 
-      <div className="secret-body">
+      <div className="secret-left-right">
         <div className="secret-image">
-          <img 
-            src="/images/finnegan.jpg" 
-            alt="Finnegan the dog" 
-            style={{ width: '150px', borderRadius: '10px' }} 
+          <img
+            src={finneganImg}
+            alt="Finnegan the dog"
+            style={{ width: "450px", borderRadius: "10px" }}
           />
         </div>
 
-        <div className="secret-skills">
-          <h3>Skills</h3>
-          <ul>
-            {secretResumeData.skills.map((skill) => (
-              <li key={skill.name}>
-                {skill.name} - {skill.level}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <div className="secret-right">
+          <div className="secret-skills">
+            <h3>Skills</h3>
+            <ul>
+              {secretResumeData.skills.map((skill) => (
+                <li key={skill.name}>
+                  {skill.name} - {skill.level}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="secret-hobbies">
-          <h3>Hobbies</h3>
-          <ul>
-            {secretResumeData.hobbies.map((hobby) => (
-              <li key={hobby}>{hobby}</li>
-            ))}
-          </ul>
+          <div className="secret-hobbies">
+            <h3>Hobbies</h3>
+            <ul>
+              {secretResumeData.hobbies.map((hobby) => (
+                <li key={hobby}>{hobby}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
 
       <div className="secret-education">
         <h3>Education</h3>
-        <h4>{secretResumeData.education.institution}</h4>
+        <h4>{secretResumeData.education.degree}</h4>
         <p>
-          {secretResumeData.education.degree} -
-          {secretResumeData.education.period}
+          {secretResumeData.education.institution} - {secretResumeData.education.period}
         </p>
+        
         <p>{secretResumeData.education.details}</p>
       </div>
     </div>
