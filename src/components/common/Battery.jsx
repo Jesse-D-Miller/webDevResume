86caad37c7781742a8c8c54935aa911a7815dbb3
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2026-01-31 09:40:19 -0800

    removed unused prop, click guard on batter now prevents jitter from rapid clicking

--- src/components/common/Battery.jsx
+++ src/components/common/Battery.jsx
@@ -2,26 +2,39 @@ import { useXP } from "../../hooks/useXP.js";
 import { useRef, useState, useEffect } from "react";
 import batteryCoverSpriteSheet from "../../assets/batteryCoverSpriteSheet.png";
 
-function Battery({ charge }) {
+function Battery() {
   const [coverOpen, setCoverOpen] = useState(false);
   const [animating, setAnimating] = useState(false);
   const [xpFloats, setXpFloats] = useState([]);
   const { xp, maxXp, grantXp, hasClicked } = useXP();
 
   const prevXp = useRef(xp);
+  const floatTimeoutRef = useRef(null);
 
   useEffect(() => {
-  if (xp > prevXp.current) {
-    const key = Date.now() + Math.random();
-    setXpFloats((prev) => [...prev, { key }]);
-    setTimeout(() => {
-      setXpFloats((prev) => prev.filter(f => f.key !== key));
-    }, 1100); 
-  }
-  prevXp.current = xp;
-}, [xp]);
+    if (xp > prevXp.current) {
+      const key = Date.now() + Math.random();
+      setXpFloats((prev) => [...prev, { key }]);
+
+      if (floatTimeoutRef.current) {
+        clearTimeout(floatTimeoutRef.current);
+      }
+
+      floatTimeoutRef.current = setTimeout(() => {
+        setXpFloats((prev) => prev.filter((f) => f.key !== key));
+      }, 1100);
+    }
+    prevXp.current = xp;
+
+    return () => {
+      if (floatTimeoutRef.current) {
+        clearTimeout(floatTimeoutRef.current);
+      }
+    };
+  }, [xp]);
 
   const handleClick = () => {
+    if (animating) return;
     setAnimating(true);
     setCoverOpen((open) => !open);
     if (!hasClicked("battery-click")) {
